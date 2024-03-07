57caa8aa5998da1682dd6fd2f62ac5c8e2eacb98
oyoo525 <simsh007@naver.com>
2024-03-07 12:41:40 +0900

    마무리 commit

--- src/components/cart_product.tsx
+++ src/components/cart_product.tsx
@@ -2,18 +2,19 @@ import React from "react";
 import './cart_product.css';
 
 export default function CartProduct(props) {
+
 	return (
 		<div className="cart_product">
 			<div className="image_box">
-				<img src=""  />
+				<img src={'/image/' + props.data.image}  />
 			</div>
 			<div className="cart_product_title">
-				<h5>name</h5>
+				<h5>{props.data.name}</h5>
 			</div>
 			<div className="cart_product_amount">
-				<div>
+				<div className="amount_btn">
 					<button>-</button>
-					<input type="number" />
+					<input type="number" value={props.data.amount} readOnly min={1} max={99}/>
 					<button>+</button>
 				</div>
 				<div className="delete_btn">
@@ -21,7 +22,7 @@ export default function CartProduct(props) {
 				</div>
 			</div>
 			<div className="cart_product_price">
-				<p>price</p>
+				<p>€ {props.data.price}</p>
 			</div>
 		</div>
 	)
--- src/page/web_site/Shop_cart.tsx
+++ src/page/web_site/Shop_cart.tsx
@@ -1,29 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './shop_cart.css';
 import Shop_nav from "../../temp/Shop_nav.tsx";
 import { useNavigate } from "react-router-dom";
 import Header from "../../temp/header/Header.tsx";
+import Footer from "../../temp/footer/Footer.tsx";
 import CartProduct from "../../components/cart_product.tsx";
 
 export default function ShopCart() {
 	const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 	const navi = useNavigate();
-	const [isEmpty, setIsEmtpy] = React.useState(true);
+	const [isEmpty, setIsEmtpy] = React.useState(false);
 
 	const cart_order = [
 		{
-			image : "",
-			name : "",
-			amount : 0,
-			price : 0
+			image : "633276-1.jpg",
+			name : "Origamo x Van Gogh Museum 3D Pop-Up Card Sunflowers mini",
+			amount : 10,
+			price : 10.66
 		},
 		{
-			image : "",
-			name : "",
-			amount : 0,
-			price : 0
+			image : "683196-1.jpg",
+			name : "Van Gogh Watch The Potato Eaters black, by Tord Boontje®",
+			amount : 1,
+			price : 99.13
 		},
 	];
+
+	const [totalPrice, setTotalPrice] = React.useState<number>(0);
+	let total_price : number = 0;
+	for(let i=0; i<cart_order.length; i++) {
+		total_price += cart_order[i].price;
+	}
+	useEffect(() => {
+		setTotalPrice(parseFloat(total_price.toFixed(2)));
+	}, [cart_order]);
 
 
 	return (
@@ -40,7 +50,7 @@ export default function ShopCart() {
 				</ul>
 			</div>
 			<div className="process_box">
-				<div>
+				<div className="step_01">
 					<i className="bi bi-1-circle"></i>Shopping cart
 				</div>
 				<div>
@@ -62,29 +72,52 @@ export default function ShopCart() {
 					{isEmpty && (
 						<p className="empty_cart">You currently do not have any products in your shopping cart.</p>
 					)}
-					
-					<div className="cart_products">
-						{cart_order.map((data) => <CartProduct data={data} />)}
+					{!isEmpty && (
+					<>
+						<div className="cart_products">
+							{cart_order.map((data) => <CartProduct data={data} />)}
+						</div>
+						<div className="total_price_box">
+							<div className="left_box">
+								<p>Express shipping to Japan (change country)</p>
+								<p>Insert gift coupon</p>
+							</div>
+							<div className="right_box">
+								<dl>Shoppoing fee</dl>
+								<dt>€ 26.00</dt>
+								<dl className="total">Total</dl>
+								<dt className="total">€ {totalPrice}</dt>
+								<div></div>
+								<div>
+									<div className="black_btn">
+										<span>NEXT <i className="bi bi-chevron-right"></i></span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</>
+				)}
+				</div>
+			</div>
+			<div className="adver_box">
+				<div className="donate_box bg_color00">
+					<h3>Donate to the Van Gogh Museum</h3>
+					<p>Your purchase supports the work of the Van Gogh Museum.</p>
+					<div className="btn_box">
+						<div className="black_btn">
+							<span>Donate € 15.00</span>
+						</div>
+						<div className="black_btn">
+							<span>Donate € 25.00</span>
+						</div>
+						<div className="black_btn">
+							<span>Donate € 50.00</span>
+						</div>
 					</div>
-
+					<p>Click on a button to add the donation to your order.</p>
 				</div>
-	
-
 			</div>
-			<div className="donate_box bg_color00">
-				<h3>Donate to the Van Gogh Museum</h3>
-				<p>Your purchase supports the work of the Van Gogh Museum.</p>
-				<div className="black_btn">
-					<span>Donate € 15.00</span>
-				</div>
-				<div className="black_btn">
-					<span>Donate € 25.00</span>
-				</div>
-				<div className="black_btn">
-					<span>Donate € 50.00</span>
-				</div>
-				<p>Click on a button to add the donation to your order.</p>
-			</div>
+			<Footer />
 		</div>
 	)
 }
